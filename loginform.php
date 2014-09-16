a0f45400a93e5bbf751d1727d0559ffe21c7e222
mranstrand <michael@Michaels-MacBook-Pro.local>
2014-09-16 12:21:51 +0200

    Formulär

---
+++ loginform.php
@@ -0,0 +1,9 @@
+<html>
+	<body>
+		<form action="login.php" method="post">
+			Användarnamn: <input type="text" name="username" /><br />
+			Lösenord: <input type="password" name="password" /><br />
+			<input type="submit" />
+		</form>
+	</body>
+</html>
